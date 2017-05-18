168dcf95303f9f213163ca0f3a7a5b63283c9f24
UnimportantWank <Xeromancer@gmail.com>
2017-05-18 15:37:33 -0500

    not sure if this is done, but committing

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import ListOfUsersContainer from './containers/ListOfUsersContainer';
 
 function App (props){
     return (
-      <div>
+      <div className="centered-wrapper">
+        <div className="centered-content">
         <ListOfUsersContainer
         filterUsers={props.filterUsers}
         chooseUser={props.chooseUser}
         users={props.users} />
         <UserDetail user={props.user} />
+        </div>
       </div>
     );
 }
